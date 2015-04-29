6954746e070a579c2e156ad0fcbabdf084b0b2b5
Truji92 <alejandro.trujillo.7@gmail.com>
2015-04-29 20:15:33 +0200

    Implementada Resta

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,4 +5,8 @@ public class HolaMundo {
     public static void main(String args) {
         System.out.println("Hola Alejandro");
     }
+
+    public int resta(int a, int b) {
+        return a-b;
+    }
 }

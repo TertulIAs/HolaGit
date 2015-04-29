1e4ddfcc0eb951e0ed37ea909174eab602cce7e0
garanda <garanda@us.es>
2015-04-29 20:42:42 +0200

    node

---
+++ src/node/Node.java
@@ -0,0 +1,17 @@
+/**
+ * Created by alejandro on 29/04/15.
+ */
+public class Node {
+    public static void main(String args) {
+        System.out.println("Hola Alejandro");
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a-b;
+
+    }
+}

5846fca2ab8c089b5dd39a07b82043cd673212a1
Truji92 <alejandro.trujillo.7@gmail.com>
2015-04-29 20:13:59 +0200

    Implementada suma

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,4 +5,8 @@ public class HolaMundo {
     public static void main(String args) {
         System.out.println("Hola Alejandro");
     }
+
+    public int suma(int a, int b) {
+        return a+b; 
+    }
 }
